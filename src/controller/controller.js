5ff10a4192006e5c3c2c49594afc2d6c77790549
Rafael Carlos De Souza <rf2014souza@gmail.com>
2021-09-21 07:18:44 -0300

    :package: Atualização 01

--- src/controller/controller.js
+++ src/controller/controller.js
@@ -3,31 +3,27 @@ class InputsKey {
 
   constructor(){
 
-    this.socketIo()
     this.elementsProtoType();
+    this.socketIo()
     this.loadElements();
 
   }
 
 
 
-loadElements(){ // Pegando todos os elementos da chat...
-
+loadElements(){ // Pegando todos os elementos pelo id da chat...
    this.dataClass = {};
 
      document.querySelectorAll('[id]').forEach(element =>{
 
       this.dataClass[Format.getCamelCase(element.id)] = element ;
 
-       return this.dataClass
-
           })
 
-          this.socketIo(this.dataClass)
+    
   }
 
 elementsProtoType(){ //Elementos para ajudar nas criações dos eventos ...
-
     Element.prototype.hide = function(){  // hide esconde
         this.style.display = 'none'
     }
@@ -37,14 +33,36 @@ elementsProtoType(){ //Elementos para ajudar nas criações dos eventos ...
     Element.prototype.toggle = function(){  // toggle se apaecer esconde , se escodeu aparece
         this.style.display = (this.style.display === 'none') ? 'block' : 'none';
     }
-
+    Element.prototype.on = function(events, fn){  // on criando eventos de click
+        events.split(' ').forEach(event =>{
+          this.addEventListener(event, fn)
+        })
+    }
     
 
   }
 
-socketIo(data){
+socketIo(){
 
-    
+  const socket = io()
+
+    this.dataClass.submit.on('click', e =>{      
+      e.preventDefault()
+      if (this.textMessage.value) {
+        socket.emit('chat message', this.textMessage.value);
+        this.textMessage.value = '';
+      }
+
+    })
+
+    socket.on('chat message', function(msg) {
+      var item = document.createElement('li');
+      item.textContent = msg;
+      this.messages.appendChild(item);
+     window.scrollTo(0, document.body.scrollHeight);
+    });
+
+
 
   }  
 

b1edfec807e630efe22f610d69ab71494fbf6b0e
Rafael Carlos De Souza <rf2014souza@gmail.com>
2021-10-25 14:02:30 -0300

    :package: carregando foto..

--- public/src/controller/controller.js
+++ public/src/controller/controller.js
@@ -70,6 +70,9 @@ initEvents(){  //eventos de div configuração e add contatos
 
       this.dataClass.addcontact.on('click', e =>{ // Click Div addcontatos..
         this.dataClass.contacts.toggle()
+        
+
+
       })
 
       this.dataClass.settings.on('click', fn=>{  // Click Div div confirações..
@@ -77,69 +80,71 @@ initEvents(){  //eventos de div configuração e add contatos
 
       })
 
-      this.dataClass.profileImg.on("click", fn =>{  // add foto
+      this.dataClass.profileImg.on("click", fn =>{  // add foto de perfil 
+        const {photo} = this.dataClass  // pega o valor do input photo
+        photo.on('change', e =>{  // quando eu clico em add foto..    
+          const file = photo.files[0] // pego o arquivo  
+          const reader = new FileReader() // crio um leitor de arquivo    
+          reader.onload = e =>{ // quando eu ler o arquivo.. 
+          this.dataClass.profileImg.src = e.target.result // eu pego o arquivo e faço o upload
 
-        this.dataClass.photo.click()
-
-      })
-
- }
-
-
-inputEventMsg(){  // evento de enviar mensagem
-    const socket = io()
-    
-
-                
-      this.dataClass.submit.on('click keypress',  e => {
-             e.preventDefault();
-             const input = this.dataClass.textMessage
-            
-             const dataUser = {
-                name: "Rafael Carlos",
-                message: input.value,
-                photo: this.dataClass.photo.value,
-                data: new Date().getDay()+ '/' + new Date().getMonth() + '/' + new Date().getFullYear(),
-                time: new Date().getHours() + ":" + new Date().getMinutes(),
-                id: socket.id
-             }   
-                                               
-              if(input.value){
-               socket.emit('chat message', dataUser)
-                 input.value = '';
-            }
+          } 
+          reader.readAsDataURL(file) // faço o upload
+        })    
       
-           const messages = this.dataClass.messages
-
-            socket.on('chat message',  (dataUser) => {
-                messages.innerHTML +=`
-                  <div id='msguser'> 
-                    <div class='date'>  ${dataUser.data} </div>                    
-                  <div class='msg-photo'> <img src='${dataUser.photo}' alt='foto'></div>
-                  <div id ='msgss'><strong>${dataUser.name}: </strong>${dataUser.message} -${(dataUser.time)}</div>
-                  </div>       
-              `
-           window.scrollTo(0, document.body.scrollHeight);
-
-             });
-     
-        })
-
-   this.dataClass.textMessage.on('keypress' , e =>{ // enviar mensagem com enter
-          if(e.key === 'Enter'){
-        this.dataClass.submit.click()
-            }
-      }) 
-  } 
-
-
-
+    })
+    
+    
+  }
+  
+  inputEventMsg(){  // evento de enviar mensagem
+      const socket = io()
+  
+                  
+        this.dataClass.submit.on('click keypress',  e => {
+          if(e.type === 'click' || e.keyCode === 13){
+               e.preventDefault();
+               const input = this.dataClass.textMessage
+              
+               const dataUser = {
+                  name: "Rafael Carlos",
+                  message: input.value,
+                  photo: this.dataClass.photo.value,
+                  data: new Date().getDay('DD')+ '/' + new Date().getMonth('MM') + '/' + new Date().getFullYear('YY'),
+                  time: new Date().getHours() + ":" + new Date().getMinutes(),
+                  id: socket.id
+               }   
+                                                 
+                if(input.value){
+                 socket.emit('chat message', dataUser)
+                   input.value = '';
+              }
+        
+             const messages = this.dataClass.messages
+  
+              socket.on('chat message',  (dataUser) => {
+                  messages.innerHTML +=`
+                    <div id='msguser'> 
+                      <div class='date'>  ${dataUser.data} </div>                    
+                    <div class='msg-photo'> <img src='${dataUser.photo}' alt='foto'></div>
+                    <div id ='msgss'><strong>${dataUser.name}: </strong>${dataUser.message} -${(dataUser.time)}</div>
+                    </div>       
+                `
+             window.scrollTo(0, document.body.scrollHeight);
+  
+               });
+       
+          } 
+          
+          })
+        
+    /*  this.dataClass.textMessage.on('keypress' , e =>{ // enviar mensagem com enter
+            if(e.key === 'Enter'){
+          this.dataClass.submit.click()
+              }
+        }) */ 
+   } 
   
 }
-
-
-
-
-
 
 

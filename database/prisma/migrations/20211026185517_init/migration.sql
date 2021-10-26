20bc56cbe4c2b3f0fd9192b7eb2c1ba410e2006f
Rafael Carlos De Souza <rf2014souza@gmail.com>
2021-10-26 16:06:02 -0300

    :package: Configurações database

---
+++ database/prisma/migrations/20211026185517_init/migration.sql
@@ -0,0 +1,11 @@
+-- CreateTable
+CREATE TABLE `Post` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+    `email` VARCHAR(255) NOT NULL,
+    `password` VARCHAR(255) NOT NULL,
+    `name` BOOLEAN NOT NULL DEFAULT false,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;

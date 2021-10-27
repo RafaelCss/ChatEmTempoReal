fff2c633632e1161b7e71dead12287c82c3b9869
Rafael Carlos De Souza <rf2014souza@gmail.com>
2021-10-26 21:43:33 -0300

    :package: database 05

--- database/index.ts
+++ database/index.ts
@@ -1,3 +1,30 @@
+import { PrismaClient } from '@prisma/client'
 
+const prisma = new PrismaClient()
+
+interface user {
+    
+    email:   string,
+    password:string,
+    name:    string
+    }
+
+async function main() {
+  const user = await prisma.post.create({
+        data: {
+        email:'rf2014@jaca.com',
+        password:'42951822',
+        name:'Rafael'
+        }
+    })
+}    
+
+main()
+  .catch((e) => {
+    throw e
+  })
+  .finally(async () => {
+    await prisma.$disconnect()
+  })
 
 
